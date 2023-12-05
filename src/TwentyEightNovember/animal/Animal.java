9498e1e0b9c501bba20ea2b999a9e640d9b5b640
lshristova20 <lshristova20@codingburgas.bg>
2023-12-05 12:08:51 +0200

    05.12 arrange the github repository

---
+++ src/TwentyEightNovember/animal/Animal.java
@@ -0,0 +1,13 @@
+package TwentyEightNovember.animal;
+
+public class Animal {
+    protected int age;
+
+    public Animal(int age){
+        this.age = age;
+    }
+
+    public void eat(){
+        System.out.println("Eeating...");
+    }
+}
